bf2a3dcef27c371e8bfcfd3ea7fa43e93678d87f
Berkay Sümer <berkay.sumer@acibadem.com.tr>
2025-03-09 22:45:06 +0300

    - seperated server, db, dbconnection and auth - changed  export method to module - added t_heart_rate entity to push datas to db

---
+++ db.js
@@ -0,0 +1,30 @@
+import client from "./dbConnection.js"; // Default export olarak client'ı import ediyoruz
+
+export const createHeartRateRecord = async (userId, rate) => {
+  try {
+    const insertQuery = `
+      INSERT INTO t_heart_rates (id, user_id, rate, create_date)
+      VALUES (gen_random_uuid(), $1, $2, NOW())
+      RETURNING *;
+    `;
+
+    const dbResult = await client.query(insertQuery, [userId, rate]);
+    return dbResult.rows[0];
+  } catch (error) {
+    console.error("createHeartRateRecord hatası:", error);
+    throw new Error("Veritabanına kaydedilemedi.");
+  }
+};
+
+export const getUserByUsername = async (username) => {
+  try {
+    const result = await client.query(
+      "SELECT * FROM t_users WHERE user_name = $1",
+      [username]
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("getUserByUsername hatası:", error);
+    throw new Error("Veritabanı hatası.");
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import axios from "axios";
+import { verifyToken, getUserIdFromToken, loginUser } from "./auth.js";
+import { createHeartRateRecord } from "./db.js";
+
+dotenv.config();
+
 const app = express();
 
 const corsOptions = {
@@ -13,13 +19,22 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
+// Send heart rate notifications to ThingsBoard
 app.post("/api/heart-rate", async (req, res) => {
-  console.log("req", req.body);
   const { rate } = req.body;
+  const token = req.headers["authorization"]?.split(" ")[1];
 
-  const token = "q9ivj64py5h1qhff3n9a";
-  const url =
-    "http://eu.thingsboard.cloud/api/v1/q9ivj64py5h1qhff3n9a/telemetry";
+  if (!token) {
+    return res.status(401).json({ message: "Token bulunamadı" });
+  }
+
+  try {
+    verifyToken(token);
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+
+  const url = process.env.HEART_RATE_URL;
 
   // prettier-ignore
   try {
@@ -36,16 +51,61 @@ app.post("/api/heart-rate", async (req, res) => {
       }
     );
 
+    const userId = getUserIdFromToken(token);
+
+    if(!userId) {
+      return res.status(401).json({message: "Geçersiz token."});
+    }
+
+    await createHeartRateRecord(userId, rate)
+
     res.status(200).json({
       message: "Veri başarılı şekilde gönderildi!",
       response: response.data,
     });
+
   } catch (error) {
     console.error("Hata:", error);
     res.status(500).json({
       message: "Server hatası.",
       error: error.message,
     });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const { user_name, password } = req.body;
+
+  if (!user_name || !password) {
+    return res
+      .status(400)
+      .json({ message: "Kullanıcı adı ve parola gereklidir" });
+  }
+
+  try {
+    const { token, first_name } = await loginUser(user_name, password);
+    return res.status(200).json({
+      message: "Giriş başarılı",
+      token,
+      first_name,
+    });
+  } catch (err) {
+    console.error("Login hatası:", err);
+
+    // TODO: err içerisine kısa kodlar eklenebilir.
+
+    if (
+      err.message === "Kullanıcı bulunamadı" ||
+      err.message === "Geçersiz parola"
+    ) {
+      return res.status(401).json({
+        message: err.message,
+      });
+    } else {
+      return res.status(500).json({
+        message: "Sunucu hatası. Lütfen daha sonra tekrar deneyin.",
+      });
+    }
   }
 });
 
