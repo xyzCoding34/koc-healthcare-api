9f658f8f37576a5bd6f4bf3bcc5b499358782370
Berkay Sümer <berkay.sumer@acibadem.com.tr>
2025-03-10 03:29:50 +0300

    - added oxygen_level_url to .env - added profile_group to login return - added createOxygenLevelRecord - dbconnection changed from client to pool

--- dbConnection.js
+++ dbConnection.js
@@ -1,17 +1,72 @@
-import pkg from "pg"; // 'pg' modülünü default olarak import et
-const { Client } = pkg; // Client'ı içeri aktarıyoruz
+import pkg from "pg";
+const { Pool } = pkg;
 
-const client = new Client({
+const pool = new Pool({
   user: "admin",
   host: "3.8.198.10",
   database: "xyzdatabase",
   password: "123321",
   port: 5432,
+  keepAlive: true,
+  connectionTimeoutMillis: 5000,
 });
 
-client
-  .connect()
-  .then(() => console.log("PostgreSQL bağlantısı başarılı"))
-  .catch((err) => console.error("PostgreSQL bağlantı hatası:", err.stack));
+const connectToDatabase = async () => {
+  try {
+    const client = await pool.connect();
+    console.log("PostgreSQL bağlantısı başarılı");
+    client.release();
+  } catch (err) {
+    console.error("PostgreSQL bağlantı hatası:", err.message);
+    setTimeout(connectToDatabase, 5000);
+  }
+};
 
-export default client; // client'ı export et
+connectToDatabase();
+
+pool.on("error", (err) => {
+  console.error(
+    "PostgreSQL bağlantısı kesildi! Yeniden bağlanıyor...",
+    err.message
+  );
+  setTimeout(connectToDatabase, 5000);
+});
+
+export default pool;
+
+// backup
+
+// import pkg from "pg";
+// const { Client } = pkg;
+
+// const client = new Client({
+//   user: "admin",
+//   host: "3.8.198.10",
+//   database: "xyzdatabase",
+//   password: "123321",
+//   port: 5432,
+//   keepAlive: true,
+//   connectionTimeoutMillis: 5000,
+// });
+
+// const connectToDatabase = async () => {
+//   try {
+//     await client.connect();
+//     console.log("PostgreSQL bağlantısı başarılı");
+//   } catch (err) {
+//     console.error("PostgreSQL bağlantı hatası:", err.message);
+//     setTimeout(connectToDatabase, 5000);
+//   }
+// };
+
+// connectToDatabase();
+
+// client.on("error", (err) => {
+//   console.error(
+//     "PostgreSQL bağlantısı kesildi! Yeniden bağlanıyor...",
+//     err.message
+//   );
+//   connectToDatabase();
+// });
+
+// export default client;
